8646ec91c45f2820f863c0314befa6b291979fae
Jakub Małyjasiak <jmalyjasiak@pgs-soft.com>
2020-07-24 13:45:57 +0200

    Add note to native build script.

--- _buildPushNative.sh
+++ _buildPushNative.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+echo "REMEMBER TO SET JAVA_HOME TO GRAALVM DISTRIBUTION AND SET [DOCKERHUB_USERNAME, DOCKERHUB_PASS] ENV VARIABLES"
+
 ./gradlew clean build -Dquarkus.container-image.push=true -Dquarkus.package.type=native -Dquarkus.container-image.username=$DOCKERHUB_USERNAME -Dquarkus.container-image.password=$DOCKERHUB_PASS
 
